98c06f6dcc175b8ba56d34e5638fccd2832f0af0
Alan Davies <MorickClive@Gmail.com>
2017-08-24 18:32:57 +0100

    Garage UI, paired script task included.

--- Javascript/Tasks.js
+++ Javascript/Tasks.js
@@ -1,9 +1,14 @@
 // Javascript Browser UI Functions
 
+	// global var (not a practice I will be repeating in the future.)
+	var david = new buildPerson("David", 19, "IT scientist");
 
 	// global functions
 	function makeBold(x){ 
 		return "<b>"+x+"</b>";
+	}
+	function pageDiv(){
+		document.write("<hr>");
 	}
 
 	function nLine(y = 1){
@@ -123,9 +128,7 @@
 		document.write("</div>");
 	}
 	
-	function challenge(){
-		var david = new buildPerson("David", 19, "IT scientist");
-		
+	function challenge(){		
 		var X = 24 + (29*2);
 		var Y = X > 50;
 
@@ -137,7 +140,7 @@
 			document.write("result : " + add(12,55) + "<br>");
 			document.write("<br>"+ (42 + 5));
 	
-		alert("result : " + add(12,55));
+		//alert("result : " + add(12,55));
 		console.log("result : " + add(12,55));
 		console.log(sq(5));
 		console.log(add3(2,3,4));
@@ -327,6 +330,134 @@
 		nLine(2);
 	}
 	
+	function createParagraph(targetID, nameAttr){
+		let para = document.createElement("p");
+		para.setAttribute("name", nameAttr);
+		let node = document.createTextNode(getText("form_paragraphText"));
+		para.appendChild(node);
+
+		let element = document.getElementById(targetID);
+		element.appendChild(para);
+	}
+	
+	function appendParagraph(targetName){
+		let val = document.getElementsByName(targetName)[0];
+		val.childNodes[0].nodeValue += getText("form_paragraphText");
+	}	
+	
+	function deleteParagraph(targetName){
+		let val = document.getElementsByName(targetName)[0];
+		if(val){val.parentNode.removeChild(val);}
+	}	
+
+	function getText(textName){
+		return document.getElementsByName(textName)[0].value;
+	}
+	
+	function domInsert(){
+		let targetVal = "script_object";
+		let objectName = "custom_para";
+		let str = "This is my function value, passed in by a variable.";
+
+		let buttons = ["para_create", "para_append", "para_del"];
+		let strfunc = [ `createParagraph("${targetVal}", "${objectName}")`,
+						`appendParagraph("${objectName}")`,
+						`deleteParagraph("${objectName}")`]
+
+		document.getElementsByName(buttons[0])[0].setAttribute("onclick", strfunc[0]);
+		document.getElementsByName(buttons[1])[0].setAttribute("onclick", strfunc[1]);
+		document.getElementsByName(buttons[2])[0].setAttribute("onclick", strfunc[2]);
+	}
+	
+	function json1(){		
+		let requestURL = "https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json";
+		
+		document.write("<p id = \"script_json\"><b>JSON 1</b></p>");
+		
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				let squadStr = ["squadName", "homeTown", "members"];
+				let requestData = JSON.parse(this.responseText);
+				
+				let parentElement = document.getElementById("script_json");
+					let myH3 = document.createElement('h3');
+					let myPara1 = document.createElement('p');
+				
+				myH3.textContent = requestData["squadName"];
+				myPara1.textContent = 	"From: " + requestData[squadStr[1]] +
+										", No of Members: " +
+										requestData[squadStr[2]].length;
+										
+				// output header						
+				parentElement.appendChild(myH3);
+				parentElement.appendChild(myPara1);
+										
+				for(let x = 0; x < requestData[squadStr[2]].length; x++){
+					let p = document.createElement('p');
+					
+					p.textContent = "Name : " + 				requestData[squadStr[2]][x]["name"] + 
+									"; Age: " + 				requestData[squadStr[2]][x]["age"] +
+									"; Secret ID: " + 			requestData[squadStr[2]][x]["secretIdentity"] +
+									"; Main special power: " +  requestData[squadStr[2]][x]["powers"][0] +
+									"; Total powers: " + 		requestData[squadStr[2]][x]["powers"].length;
+					
+					parentElement.appendChild(p);
+				}
+				// debug // console.log(requestData);	
+			}
+		};
+		xhttp.open("GET", requestURL, true);
+		xhttp.send();
+	}
+	
+	// tragic code, needs re-evaluation/re-implemetation do more flexable use.
+	function json2_Search(){
+		let requestURL = "https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/kings.json";
+		
+		let xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				
+				let requestData = JSON.parse(this.responseText);
+				let jsonSize = requestData.length;
+				let val = document.getElementsByName("form_jsonSearch")[0].value;
+				
+				let parentElement = document.getElementById("script_json2");
+					let myH3 = document.createElement('h3');
+					let myPara1 = document.createElement('p');
+				
+				
+				myH3.textContent = "Search results:";
+				for(let x = 0; x < jsonSize; x++){
+					if(requestData[x]["nm"].includes(val)){
+						myPara1.textContent += "Name : " + requestData[x]["nm"] + ", ";
+						myPara1.textContent += "City : " + requestData[x]["cty"]+ ", ";
+						myPara1.textContent += "House : " + requestData[x]["hse"]+ ", ";
+						myPara1.textContent += "Years : " + requestData[x]["yrs"]+ ".";
+						break;
+					}				
+				}
+				parentElement.appendChild(myH3);
+				parentElement.appendChild(myPara1);
+				// debug // console.log(requestData);	
+			}
+		};
+		xhttp.open("GET", requestURL, true);
+		xhttp.send();
+	}
+	
+	// see above.
+	function json2(){
+		document.write("<p id = \"script_json2\"><b>JSON 2</b></p>");
+		document.write(	"<table><tr>"+
+						"<td>Search by name: </td>"+
+						"<td><input type=\"text\" name=\"form_jsonSearch\"></td>"+
+						"<td><button onclick= \"json2_Search()\">Search</button></td>"+
+						"</tr></table>");
+	}
+	
+	
 	function buildCar(model, reg, val, faults){ 
 		this.model = model;
 		this.reg = reg;
@@ -356,8 +487,7 @@
 				document.write("Vehicle added!<br> NEW! : ");
 				document.write(this.vehicleList[this.vehicleList.length-1].printMe());
 			};
-		this.checkCar_out = function(reg){
-			
+		this.checkCar_out = function(reg){			
 			for(let x = 0; x < this.vehicleList.length; x++){
 				if(this.vehicleList[x].reg === reg){
 					document.write("Vehicle " + this.vehicleList[x].reg +
@@ -368,14 +498,92 @@
 		
 					break;
 				}
+			}
+		this.listVehicles_Form = function(targID){
+			let parentElement = document.getElementById(targID);			
+			
+			while (parentElement.hasChildNodes()) {
+				parentElement.removeChild(parentElement.lastChild);
+			}
+			
+			
+			for(let x = 0; x < this.vehicleList.length; x++){
+				let myPara = document.createElement('p');
+				myPara.textContent = this.vehicleList[x].printMe();
 				
+				parentElement.appendChild(myPara);
+			}
+			
+		};
+		this.checkCar_inForm = function(formID){
+				let model = document.getElementsByName(formID + ".model")[0].value;
+				let reg = 	document.getElementsByName(formID + ".reg")[0].value;
+				let val = 	document.getElementsByName(formID + ".val")[0].value;
+				let faults = document.getElementsByName(formID + ".faults")[0].checked;
+							
+			
+				this.vehicleList[this.vehicleList.length] = new buildCar(model, reg, val, faults);
+				alert("Vehicle added: \n" + this.vehicleList[this.vehicleList.length-1].printMe());
+			};			
+		};
+		this.listVehicle_Form = function(formID, targID){
+			let parentElement = document.getElementById(targID);			
+			let value = document.getElementsByName(formID + ".regSearch")[0].value;
+			
+			// clean up
+			while (parentElement.hasChildNodes()) {
+				parentElement.removeChild(parentElement.lastChild);
+			}
+			
+			let myPara = document.createElement('p');
+			for(let x = 0; x < this.vehicleList.length; x++){
+				if(this.vehicleList[x].reg === value){
+					myPara.textContent = this.vehicleList[x].printMe();
+					break;
+				}
+			}
+				
+			parentElement.appendChild(myPara);
+			
+		};
+		this.valCar_Form = function(formID, targID){
+			let parentElement = document.getElementById(targID);			
+			let value = document.getElementsByName(formID + ".regSearch")[0].value;
+			
+			// clean up
+			while (parentElement.hasChildNodes()) {
+				parentElement.removeChild(parentElement.lastChild);
+			}
+			
+			let myPara = document.createElement('p');
+			for(let x = 0; x < this.vehicleList.length; x++){
+				if(this.vehicleList[x].reg === value){
+					myPara.textContent = "The value of "+this.vehicleList[x].model+", reg: " + this.vehicleList[x].reg + ", is: " + this.vehicleList[x].rtnTotalPrice();
+					break;
+				}
+			}
+				
+			parentElement.appendChild(myPara);
+		};
+		this.delCar_Form = function(formID){			
+			let value = document.getElementsByName(formID + ".regSearch")[0].value;
+			
+			for(let x = 0; x < this.vehicleList.length; x++){
+				if(this.vehicleList[x].reg === value){
+					alert("Vehicle " + this.vehicleList[x].reg +
+					" of model type: " + this.vehicleList[x].model +
+					" has been removed from the garage!");
+					delete this.vehicleList[x];
+					this.vehicleList.splice(x,1);
+				}
 			}			
 		};
 		this.carBillCalc = function(index){return this.vehicleList[index].rtnTotalPrice();};
 	}
 	
+	// need access globally
+	var myGarage = new buildGarage("Alex's Garage shop");
 	function garage(){
-		var myGarage = new buildGarage("Alex's Garage shop");
 		// This section needs to upgraded, particularly to include form entry/UI driven.
 		document.write(makeBold("<u>Garage section:</u>"))
 		nLine(1);
@@ -386,52 +594,14 @@
 		document.write("Car value: " + myGarage.carBillCalc(0)); nLine(1);
 		myGarage.checkCar_out("CR4-Z87");
 		myGarage.listVehicles();
+		myGarage.checkCar_in("Volvo", "CR4-Z87", 2200, true);
 	}
-	
-	function createParagraph(targetID, nameAttr){
-		let para = document.createElement("p");
-		para.setAttribute("name", nameAttr);
-		let node = document.createTextNode(getText("form_paragraphText"));
-		para.appendChild(node);
-
-		let element = document.getElementById(targetID);
-		element.appendChild(para);
-	}
-	
-	function appendParagraph(targetName){
-		let val = document.getElementsByName(targetName)[0];
-		val.childNodes[0].nodeValue += getText("form_paragraphText");
-	}	
-	
-	function deleteParagraph(targetName){
-		let val = document.getElementsByName(targetName)[0];
-		if(val){val.parentNode.removeChild(val);}
-	}	
-
-	function  getText(textName){
-		return document.getElementsByName(textName)[0].value;
-	}
-	
-	function domInsert(){
-		let targetVal = "script_object";
-		let objectName = "custom_para";
-		let str = "This is my function value, passed in by a variable.";
-
-		let buttons = ["para_create", "para_append", "para_del"];
-		let strfunc = [ `createParagraph("${targetVal}", "${objectName}")`,
-						`appendParagraph("${objectName}")`,
-						`deleteParagraph("${objectName}")`]
-
-		document.getElementsByName(buttons[0])[0].setAttribute("onclick", strfunc[0]);
-		document.getElementsByName(buttons[1])[0].setAttribute("onclick", strfunc[1]);
-		document.getElementsByName(buttons[2])[0].setAttribute("onclick", strfunc[2]);
-	}
-	
 	
 	function intermediate(){
-		document.write("<b>Intermediate!</b>")	;	
-		nLine(2); // creates multiple doc new line prints
+		document.write("<b>Intermediate!</b>");	
+		pageDiv();
 		
+		nLine(2); // creates multiple doc new line prints
 		// Task 14 - add FIZZBUZZ logic
 		fizzBUZZ();
 		// Task 14 b - add 3 parameters, 1 that acts as the max loop iteration/
@@ -439,25 +609,34 @@
 		//			 - 1 that acts as a representation of the word divisible by 5.
 		fizzBUZZ_bonus(25,"Dog", "Class");
 	
+		pageDiv();
 		// Task 15 - Create a method that divides a given number until it divides by 3.
 		//			 Where the result is 1, adjust the value to ensure each value is perfectly
 		//			 divisible by 3 by adding or removing 1.
 		iteration4(50);
+		pageDiv();
 		
 		// Task 16 - Write a function that counts overlapping triple characters in a string.
 		//			 return a value that represents the number of detected triples.
 		strings4();
+		pageDiv();
 		// Task 17 - DOM 1
 		document.write(makeBold("See <u>Paragraph control form</u>"));
 		nLine(2);
 		domInsert();
+		pageDiv();
 		
 		// Task 18 - JSON 1
-		//json1();
+		json1();
+		pageDiv();
+		
 		// Task 19 - JSON 2
+		json2();
+		pageDiv();
 		
 		// Task 20 - Garage
 		garage();
+		
 		// Task 21 - Admin Interface
 	}
 	
